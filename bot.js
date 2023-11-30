4270e8442bff8910d913933e5d1cd52bf1cf442e
sam hockaday <samuel.hockaday@gmail.com>
2023-11-30 20:38:25 +0000

    Working hal 9000 ai bot

--- bot.js
+++ bot.js
@@ -1,74 +1,113 @@
+// import { Client, GatewayIntentBits } from "discord.js";
+// import { config as dotenvConfig } from 'dotenv';
 // import OpenAI from "openai";
-import { OpenAIApi, Configuration } from "openai";
-// console.log(OpenAI.Assistants)
+
+// dotenvConfig();
+
+// const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
+// const token = process.env.DISCORD_TOKEN;
+
+// const client = new Client({
+//     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+// });
+
+// client.once('read', () => {
+//   console.log('Hal 9000 is now O N L I N E! ');
+// })
+
+// client.on('messageCreate', async (msg) => {
+//     if (msg.author.bot) return;
+    
+// const processedContent = msg.content.trim();
+//     try {
+//         const stream = await openai.chat.completions.create({
+//             model: "gpt-4",
+//             messages: [{ role: "user", content: msg.content }],
+//             stream: true,
+//             temperature: 0,
+//             top_p: 0.95,
+
+//         });
+
+//         let responseContent = "";
+//         let timeoutId;
+
+//         const collectResponse = async () => {
+//             for await (const chunk of stream) {
+//                 responseContent += chunk.choices[0]?.delta?.content || "";
+//                 clearTimeout(timeoutId);
+//                 timeoutId = setTimeout(() => {
+//                     if (responseContent) {
+//                         msg.reply(responseContent);
+//                         console.log(`Replied to ${msg.author.username}: ${responseContent}`);
+//                     }
+//                 }, 500); // Wait for 500ms for additional chunks
+//             }
+//         };
+
+//         collectResponse();
+
+//     } catch (error) {
+//         console.error("Error asking OpenAI:", error);
+//         msg.reply("Apologies, Dave. But that is not possible right now.");
+//     }
+// });
+
+// client.login(token);
 
 
-import { Client, GatewayIntentBits } from "discord.js"
+//require('dotenv/config');
+// const { Client } = require('discord.js');
+// const { OpenAIApi } = require('openai');
 
-import { config as dotenvConfig } from 'dotenv';
-dotenvConfig();
-const token = process.env.DISCORD_TOKEN;
+import dotenv from 'dotenv';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { OpenAI } from 'openai';
 
-const openaiApiKey = process.env.OPEN_API_KEY;
-const configuration = new Configuration({
-    apiKey: openaiApiKey,
-});
-
-//async code 
-console.log(OpenAI)
-
-async function ask(prompt){
-    const response = await  OpenAI.createCompletion({
-        model: "gpt-3.5-turbo",
-        prompt,
-
-
-})
-
-const answer =response.data.choices[0].text;
-console.log(answer); 
-};
-ask('hello')
-
-if (!token) {
-    console.error("Please provide a valid Discord bot token in the .env file.");
-    process.exit(1);
-}
-
-
-
-
+dotenv.config();
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.on("ready", () => {
-    console.log("Hal 9000 is online") //when hal is online
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_API_KEY,
 });
-client.login(token);
 
+client.once('ready', () => {
+  console.log('Bot is online');
+});
 
-// Create an object to store whether the bot has replied to each user
-const repliedUsers = {};
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
 
+  const userMessage = message.content;
 
-client.on('messageCreate', (msg) => {
-    console.log(`Received message from ${msg.author.username}: ${msg.content}`);
-    
-    // Check if the message is from a user and not the bot itself
-    if (msg.author.bot) return;
+  console.log(userMessage);
 
-    // Check if the user has already received a reply
-    if (!repliedUsers[msg.author.id]) {
-        // Send the reply
-        msg.reply('Hello Dave. You are looking well today.');
+  try {
+    const stream = await openai.chat.completions.create({
+      model: "gpt-4",
+      messages: [{ role: "user", content: userMessage }],
+      stream: true,
+    });
 
-        // Set the flag to indicate that the user has received a reply
-        repliedUsers[msg.author.id] = true;
+    let botReply = '';
+    for await (const chunk of stream) {
+      botReply += chunk.choices[0]?.delta?.content || '';
     }
+
+    if (botReply) {
+      message.reply(botReply.trim());
+    }
+  } catch (error) {
+    console.error('OpenAI Error:', error.message);
+    message.reply("Apologies, but I'm unable to respond right now.");
+  }
 });
 
+client.login(process.env.DISCORD_TOKEN);
